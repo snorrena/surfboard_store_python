e07440c57435b6fbbb3cae7cb377333c65df64bf
Scott Norrena <snorrena@gmail.com>
2025-12-28 18:23:13 -0500

    formated code with Ruff and added .gitignore for __pycache__ folder

--- display.py
+++ display.py
@@ -1,6 +1,6 @@
 from products import Surfboard
 
 
-def print_surfboard_data(surfboards: [Surfboard]):
+def print_surfboard_data(surfboards: list[Surfboard]):
     for surfboard in surfboards:
         surfboard.print_info()
