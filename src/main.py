92c160f2ab608e1d3d0feace3f697b26d6314b96
Scott Norrena <snorrena@gmail.com>
2026-01-14 21:18:58 -0500

    changed forward slash to number sign for todo comment on main.py

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from inventory import surfboard_list
 from display import print_surfboard_data
 from products import Surfboard
 
-//TODO: this is a second todo added to test lazygit
+# /TODO: this is a second todo added to test lazygit
 
 surfboard: Surfboard
 for surfboard in surfboard_list:
